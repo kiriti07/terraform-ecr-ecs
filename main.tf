6649b3f73c9551f5cd424c2709408aa3da006b3d
kiriti07 <ch.kiriti@gmail.com>
2023-06-20 01:02:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-# Define variables
-variable "aws_region" {
-  type    = string
-  default = "us-east-1" # Update with your desired AWS region
-}
-
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16" # Update with your desired VPC CIDR block
-}
-
-variable "public_subnet_cidr" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"] # Update with your desired public subnet CIDR blocks
-}
-
-variable "private_subnet_cidr" {
-  type    = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"] # Update with your desired private subnet CIDR blocks
-}
-
 # Create VPC, subnets, and security groups
 resource "aws_vpc" "my_vpc" {
   cidr_block           = var.vpc_cidr
