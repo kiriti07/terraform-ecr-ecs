26ecd8d4f0566cc8cd50dd4f7c3cbff6c9846b8e
kiriti07 <ch.kiriti@gmail.com>
2023-06-20 00:57:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,120 @@
+# Define variables
+variable "aws_region" {
+  type    = string
+  default = "us-east-1" # Update with your desired AWS region
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16" # Update with your desired VPC CIDR block
+}
+
+variable "public_subnet_cidr" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"] # Update with your desired public subnet CIDR blocks
+}
+
+variable "private_subnet_cidr" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"] # Update with your desired private subnet CIDR blocks
+}
+
+# Create VPC, subnets, and security groups
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_subnet" "public_subnet" {
+  count                 = length(var.public_subnet_cidr)
+  vpc_id                = aws_vpc.my_vpc.id
+  cidr_block            = var.public_subnet_cidr[count.index]
+  availability_zone     = "us-east-1a" # Update with your desired availability zone
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  count                 = length(var.private_subnet_cidr)
+  vpc_id                = aws_vpc.my_vpc.id
+  cidr_block            = var.private_subnet_cidr[count.index]
+  availability_zone     = "us-east-1b" # Update with your desired availability zone
+}
+
+resource "aws_security_group" "ecs_security_group" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  # Allow inbound traffic from the public subnet CIDR blocks
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.public_subnet_cidr
+  }
+
+  # Allow outbound traffic to the internet
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create IAM roles for ECS task execution and ECS service
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_service_role" {
+  name = "ecs-service-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Create ECR repository
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name = "my-ecr-repo"
+}
+
+# Create ECS cluster
+resource "aws_ecs_cluster" "my_ecs_cluster" {
+  name = "my-ecs-cluster"
+
+  capacity_providers = ["FARGATE"]
+}
+
+# Output the ECR repository URL
+output "ecr_repository_url" {
+  value = aws_ecr_repository.my_ecr_repo.repository_url
+}
